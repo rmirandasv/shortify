c2a8f65fcb2405b3e2adf66918d0f31cd462b649
Ronald Miranda <ronaldmirandasv@gmail.com>
2024-01-19 02:25:10 -0600

    homepage dashboard link if auth

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,8 +2,13 @@
     <nav class="py-3 px-4 lg:px-10 flex justify-between bg-transparent">
         <span class="text-2xl font-bold">{{ config('app.name') }}</span>
         <div class="flex items-center space-x-4">
-            <a wire:navigate href="{{ route('login') }}" class="text-gray-100 hover:text-white">Login</a>
-            <a wire:navigate href="{{ route('register') }}" class="text-gray-100 hover:text-white">Register</a>
+            @auth
+                <a wire:navigate href="{{ route('dashboard') }}" class="text-gray-100 hover:text-white">Dashboard</a>
+            @endauth
+            @guest
+                <a wire:navigate href="{{ route('login') }}" class="text-gray-100 hover:text-white">Login</a>
+                <a wire:navigate href="{{ route('register') }}" class="text-gray-100 hover:text-white">Register</a>
+            @endguest
         </div>
     </nav>
     <div class="h-full flex flex-col items-center">
