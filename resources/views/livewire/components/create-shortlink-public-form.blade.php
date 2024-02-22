507cb64e17af106344579cd092208c82a510b77a
Ronald Miranda <ronaldmirandasv@gmail.com>
2024-02-22 17:53:28 -0600

    fix colspan when no records found

--- resources/views/livewire/components/create-shortlink-public-form.blade.php
+++ resources/views/livewire/components/create-shortlink-public-form.blade.php
@@ -4,7 +4,7 @@
             <label for="url" class="text-xl font-bold text-white">Enter your URL</label>
             <div class="flex items-center">
                 <input type="text" name="url" id="url" x-model="url" wire:model="url"
-                    class="mt-2 px-4 py-2 w-full rounded-md bg-gray-100 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent" />
+                    class="px-4 py-2 w-full rounded-md bg-gray-100 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent" />
 
                 <button type="submit" x-bind:disabled="!url"
                     class="ml-2 px-4 py-2 rounded-md bg-gray-700 text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent disabled:opacity-50 disabled:cursor-not-allowed">
