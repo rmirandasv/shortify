119ec9facea307166b68afc4bd995cbc4c06a5fd
Ronald Miranda <ronaldmirandasv@gmail.com>
2024-05-12 22:31:05 -0600

    basic seo

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,6 +5,22 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name') }}</title>
+    <meta name="title" content="{{ config('app.name') }}">
+    <meta name="description" content="Free and open source URL shortener">
+    <meta name="author" content="Ronald Miranda">
+    <meta name="image" content="{{ asset('images/shortify-screenshoot-2.png') }}">
+    <meta property="og:title" content="{{ config('app.name') }}">
+    <meta property="og:description" content="Free and open source URL shortener">
+    <meta property="og:image" content="{{ asset('images/shortify-screenshoot-2.png') }}">
+    <meta property="og:url" content="{{ url()->current() }}">
+    <meta property="og:site_name" content="{{ config('app.name') }}">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="{{ config('app.name') }}">
+    <meta name="twitter:description" content="Free and open source URL shortener">
+    <meta name="twitter:image" content="{{ asset('images/shortify-screenshoot-2.png') }}">
+    <meta name="twitter:site" content="@r_miranda9">
+    <meta name="twitter:creator" content="@r_miranda9">
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
     @vite('resources/css/app.css')
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
